3db5fec0f90b6578fb60dfe276e1e1731ce24b03
DovletBegenjev <56294209+DovletBegenjev@users.noreply.github.com>
2021-12-15 16:29:07 +0300

    Update TrigFunc_Kursovaya_4kurs.cpp

--- TrigFunc_Kursovaya_4kurs/TrigFunc_Kursovaya_4kurs.cpp
+++ TrigFunc_Kursovaya_4kurs/TrigFunc_Kursovaya_4kurs.cpp
@@ -2,58 +2,111 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 #include <math.h>
 #include <ctime>
 using namespace std;
+#define M_PI 3.14159265358979323846
 
-#include "sercantutar-infint-fc767ed/InfInt.h"
-
-int main()
+int main(int argc, char** argv)
 {
-	InfInt myint1;
-    InfInt myint2;
+	int rank, size;
+	MPI_Status status;
 
-	// n <= 110
-	int n = 110, i = 0, j = 0, temp = 0, a = -1;
-	double denom = 1;
-	double x = 5, sinus = 0, cosinus = 0;
+	// can't count pi, pi/2, 3*pi/2, 2*pi 
+	int i = 0, j = 0, temp = 0, a = -1, rows = 300;
+
+	double x = 5 * M_PI / 6;
+	double e = 0; // e <= -323
+
+	double denom = 1, b = x, sinus = 0, cosinus = 0;
 
 	/*cout << "x = ";
 	cin >> x;*/
 
-	unsigned int start_time = clock();
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size); //кол-во запущен процессов (size)
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	// sin
-	for (i = 0; i < n; i++)
+	double t1 = MPI_Wtime();
+
+	int k = rows / size;
+	int start = rank * k; // если 1 процесс то стартовая позиция = 25
+	int stop = (rank + 1) * k;
+	if (rank == size - 1) stop = rows;
+
+	for (j = start; j < stop; j++)
 	{
-		temp = 2 * i + 1;
-		if (i > 0)
+		if (rank == 0)
 		{
-			denom *= (temp - 1) * temp; // факториал denom = (2n+1)!
+			e = pow(10, -j);
+			a = -1;
+			denom = 1;
+			b = x;
+			i = 0;
+			sinus = cosinus = 0;
 		}
-		a *= -1;
 
-		sinus += (a * pow(x, temp)) / denom;
+		// sin
+		while (abs(b) > e)
+		{
+			temp = 2 * i + 1;
+			if (i > 0)
+			{
+				denom *= (temp - 1) * temp; // факториал denom = (2n+1)!
+				a *= -1;
+			}
+			b = (a * pow(x, temp)) / denom;
+			sinus += b;
+			++i;
+		}
+
+		if (rank == 0)
+		{
+			a = -1;
+			denom = 1;
+			b = x;
+			i = 0;
+		}
+
+		// cos
+		while (abs(b) > e)
+		{
+			temp = 2 * i;
+			if (i > 0)
+			{
+				denom *= (temp - 1) * temp; // факториал denom = (2n+1)!
+				a *= -1;
+			}
+			b = (a * pow(x, temp)) / denom;
+			cosinus += b;
+			++i;
+		}
 	}
 
-	// cos
-	a = -1;
-	denom = 1;
-	for (i = 0; i < n; i++)
+	double total_sin = 0, total_cos = 0;
+
+	MPI_Reduce(&sinus, &total_sin, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&cosinus, &total_cos, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	MPI_Bcast(&total_sin, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&total_cos, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	double t2 = MPI_Wtime() - t1;
+
+	if (rank == 0)
 	{
-		temp = 2 * i;
-		if (i > 0)
-		{
-			denom *= (temp - 1) * temp; // факториал denom = (2n+1)!
-		}
-		a *= -1;
-		cosinus += (a * pow(x, temp)) / denom;
+		cout << "sinus = " << -sinus << endl;
+		cout << "sin(x) = " << sin(x) << endl << endl;
+
+		cout << "cosinus = " << -cosinus << endl;
+		cout << "cos(x) = " << cos(x) << endl << endl;
+
+		cout << "time = " << t2 << endl;
 	}
 
-	unsigned int end_time = clock();
-	unsigned int search_time = end_time - start_time;
-
-	cout << "sinus = " << sinus << endl;
-	cout << "cosinus = " << cosinus << endl;
-	cout << "time = " << search_time / 1000 << endl;
+	MPI_Finalize();
+	return 0;
 }
